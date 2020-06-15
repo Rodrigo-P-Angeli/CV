301554bbbb8cccd233e7db197bc2564764824502
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-06-15 13:43:44 +0100

    Botton-Tab Added

---
+++ CV/src/Navigation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import App from '../App'
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={App} />
+        <Tab.Screen name="Settings" component={App} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
