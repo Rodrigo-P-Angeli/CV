80c337f73d4739c8a8ae9c5396d82e03f2b20c99
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-06-19 21:13:28 +0100

    Update Navigation.js

--- CV/src/Navigation.js
+++ CV/src/Navigation.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
