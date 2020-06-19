525f8e657c8826072a3d66698de265b2b0d73769
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-06-19 14:56:49 +0100

    Update Escolaridade.js

--- CV/src/Components/Escolaridade.js
+++ CV/src/Components/Escolaridade.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import { View, Image, Text, StyleSheet } from 'react-native'
+import { View, Image, Text, StyleSheet, ScrollView } from 'react-native'
 
 
 export default class Escolaridade extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Image source={this.props.image} />
-                
+                <ScrollView>
+                    <Image source={this.props.image} />
+                    <Text style={styles.periodo}>{this.props.periodo}</Text>
+                    <Text style={styles.curso}>{this.props.curso}</Text>
+                </ScrollView>
             </View>
         )
     }
@@ -15,11 +18,17 @@ export default class Escolaridade extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
-        alignItems: 'center'
+        //justifyContent: 'center',
+        //alignItems: 'center',
+        padding: 10,
     },
     image: {
         backgroundColor: 'white',
         alignSelf: 'center',
+    },
+    periodo: {
+        fontWeight: 'bold',
+        fontSize: 20,
     }
+
 })
