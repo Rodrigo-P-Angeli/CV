9fc9bfa43cdeadfdc066397977425c2cb075efb7
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-06-24 15:58:28 +0100

    Update MenuDrawer.js

--- CV/src/Components/MenuDrawer.js
+++ CV/src/Components/MenuDrawer.js
@@ -1,41 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 import {
     DrawerContentScrollView,
     DrawerItemList,
 } from '@react-navigation/drawer'
 
+import axios from 'axios'
+
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Email from 'react-native-vector-icons/MaterialCommunityIcons'
 
-export default function (props) {
-    return (
-        <View style={{ flex: 1 }}>
-            <View style={styles.container}>
-                <Text style={styles.title}>CV Rodrigo Pissinate</Text>
-                <View style={styles.content}>
-                    <Image style={styles.image} source={require('../assets/images/foto_perfil.jpg')} />
-                    <View style={{flex:1}}>
-                        <View style={styles.itens}>
-                            <Email name={'email'} size={15} />
-                            <Text style={styles.contato}>rodrigop.deangeli@gmail.com</Text>
-                        </View>
-                        <View style={styles.itens}>
-                            <Icon name={'phone'} size={15} />
-                            <Text style={styles.contato}>+353 87 351 8144</Text>
-                        </View>
-                        <View style={styles.itens}>
-                            <Icon name={'phone'} size={15} />
-                            <Text style={styles.contato}>+55 27 99992 8105</Text>
+
+
+export default class menuDrawer extends Component {
+    state = {}
+    loadInfo() {
+        axios.get('https://lambe-e09e6.firebaseio.com/profile.json')
+            .then(res => this.setState(res.data))
+            .catch(err => Alert.alert('Ops...', err))
+    }
+    componentDidMount() {
+        this.loadInfo()
+    }
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>CV Rodrigo Pissinate</Text>
+                    <View style={styles.content}>
+                        <Image style={styles.image} source={require('../assets/images/foto_perfil.jpg')} />
+                        <View style={{ flex: 1, justifyContent: 'space-around' }}>
+                            <View style={styles.itens}>
+                                <Email name={'email'} size={15} />
+                                <Text style={styles.contato}>{this.state.email}</Text>
+                            </View>
+                            <View style={styles.itens}>
+                                <Icon name={'phone'} size={15} />
+                                <Text style={styles.contato}>{this.state.phone}</Text>
+                            </View>
+                            {/* <View style={styles.itens}>
+                                <Icon name={'phone'} size={15} />
+                                <Text style={styles.contato}>+55 27 99992 8105</Text>
+                            </View> */}
                         </View>
                     </View>
                 </View>
-            </View>
-            <DrawerContentScrollView {...props}>
-                <DrawerItemList {...props} />
-            </DrawerContentScrollView>
-        </View>
-    )
+                <DrawerContentScrollView {...this.props}>
+                    <DrawerItemList {...this.props} />
+                </DrawerContentScrollView>
+            </View >
+        )
+    }
 }
 
 const styles = StyleSheet.create({
