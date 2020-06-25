32e5e2ec6f5a83560a261ddb4df3cbea93a7f470
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-06-25 20:15:20 +0100

    Update Motivation.js

--- CV/src/Screens/Motivation.js
+++ CV/src/Screens/Motivation.js
@@ -23,20 +23,19 @@ export default class Conhecimentos extends Component {
                             {'\nTambém serviu para que eu pudesse aumentar meu portifólio e aprender a trabalhar em conjunto com o GitHub.\n'}
                         </Text>
                         <TouchableOpacity style={styles.link} onPress={() => { Linking.openURL('https://github.com/Rodrigo-P-Angeli/CV/tree/master/CV') }}>
-                            <Text style={styles.textLink}>Link para Git do Projeto</Text>
+                            <Text style={styles.textLink}>Click para acessar o Git do Projeto</Text>
                         </TouchableOpacity>
                         <Topicos topico={'Descrição do projeto'} />
                         <Text style={styles.content}>
                             {'\nEste projeto tem por caracteristica mostrar minha capacidade criar um App com a biblioteca mais atual de navegação '}
                             {'do react-navigation, assim como apresentar meus outros projetos do meu GitHub.'}
-                            {' Usei o bottom tab navigation e o drawer para navegar entre as tela como é observado, e o switch navigation'}
-                            {' para navegar na splash screen. '}
+                            {' Usei o bottom tab navigation e o drawer para navegar entre as tela como é observado.'}
                             {'\n\nO firebase do Google também foi usado no projeto para que ao longo do tempo ele esteja sempre atualizado e não'}
                             {' seja necessário lançar novas versões do aplicativo, as experiências, cursos e certificados são obtidos '}
                             {'diretamente do servidor\n'}
                         </Text>
                         <TouchableOpacity style={styles.link} onPress={() => { Linking.openURL('https://github.com/Rodrigo-P-Angeli/') }}>
-                            <Text style={styles.textLink}>Link para meu GitHub{'\n\n'}</Text>
+                            <Text style={styles.textLink}>Click para acessar meu GitHub{'\n\n'}</Text>
                         </TouchableOpacity>
                     </ScrollView>
                 </ImageBackground>
