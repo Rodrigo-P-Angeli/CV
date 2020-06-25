5ab49ef9318e09e589c7aade211414d0101668b8
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-06-25 21:05:47 +0100

    Creating Job

---
+++ CV/src/Components/Job.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export default class Job extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        flex: 1
+    },
+})
--- CV/src/Screens/OtherJobs.js
+++ CV/src/Screens/OtherJobs.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, Alert } from 'react-native'
+import axios from 'axios'
+
 import Header from '../Components/Header'
+import Job from '../Components/Job'
 
 export default class OtherJobs extends Component {
+    state = {
+        trabalhos: []
+    }
+
+    componentDidMount() {
+        this.loadingOtherJobs()
+    }
+
+    loadingOtherJobs() {
+        axios.get('https://lambe-e09e6.firebaseio.com/otherjobs.json')
+            .then(res => this.setState({ trabalhos: res.data }))
+            .catch(err => Alert.alert('Opss.', err))
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Header {...props} />
-                <FlatList
-                
-                />
+                <ImageBackground source={require('../assets/images/back-ground.jpg')} style={styles.backround}>
+                    <Header {...this.props} subtitle={'Outros trabalhos'} />
+                    <FlatList
+                        keyExtractor={Math.random()}
+                        data={this.state.trabalhos}
+                        renderItem={({ item }) => <Job {...item} />}
+                    />
+                </ImageBackground>
             </View>
         )
     }
@@ -18,5 +39,10 @@ export default class OtherJobs extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    backround: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
     }
 })
