fa62eab97178e39bd45cbce5f052ba3b7b72f52a
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-06-26 19:45:41 +0100

    Update Job.js

--- CV/src/Components/Job.js
+++ CV/src/Components/Job.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity, Linking } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 export default class Job extends Component {
     render() {
         return (
-            <TouchableOpacity style={styles.container} onPress={() => Linking.openURL(this.props.link)}>
+            <View style={styles.container}>
                 <Text style={styles.name}>{this.props.name}</Text>
-                <Text style={styles.link}>{this.props.link}</Text>
-            </TouchableOpacity>
+                <View style={styles.icons}>
+                    {this.props.linkGit? <TouchableOpacity onPress={() => Linking.openURL(this.props.linkGit)}>
+                        <FontAwesome name={'github'} size={30} />
+                    </TouchableOpacity> : <FontAwesome name={'github'} size={30} color={'#999'}/>}
+                    {this.props.linkAndroid ? <TouchableOpacity onPress={() => Linking.openURL(this.props.linkAnroid)}>
+                        <FontAwesome name={'android'} size={30} />
+                    </TouchableOpacity> : <FontAwesome name={'android'} size={30} color={'#999'} />}
+                    {this.props.linkIos ? <TouchableOpacity onPress={() => Linking.openURL(this.props.linkIos)}>
+                        <FontAwesome name={'apple'} size={30} />
+                    </TouchableOpacity> : <FontAwesome name={'apple'} size={30} color={'#999'} />}
+                </View>
+            </View>
         )
     }
 }
@@ -15,16 +26,24 @@ export default class Job extends Component {
 const styles = StyleSheet.create({
     container: {
         padding: 10,
-        flex: 1
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     name: {
         fontFamily: 'Solway-Bold',
         fontSize: 20,
         paddingBottom: 10,
+        flex: 3,
     },
     link: {
         fontFamily: 'Solway-Light',
         fontSize: 15,
         textAlign: 'justify'
+    },
+    icons: {
+        flexDirection: 'row',
+        flex: 1,
+        justifyContent: 'space-between',
     }
 })
